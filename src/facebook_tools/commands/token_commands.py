58ef73c95f61c9d445abbd1181d43c2291e4626b
Osmay Cruz <osmay.cruz@gmail.com>
2020-10-21 14:42:28 -0400

    Updated for viewing credential

--- src/facebook_tools/commands/token_commands.py
+++ src/facebook_tools/commands/token_commands.py
@@ -1,4 +1,5 @@
 import click
+import json
 from facebook_tools import token_utils
 from facebook_tools.token_utils import (get_page_access_token, get_user_long_lived_token, debug_access_token)
 from facebook_tools.cred_utils import store_credentials, get_credentials
@@ -6,11 +7,16 @@ from facebook_tools.messaging import send_text_dm
 
 
 @click.command(help="Store the app credentials locally")
-def creds():
-    app_id = click.prompt("Facebook app_id: ")
-    app_secret = click.prompt("Facebook app_secret: ")
-    click.echo("Storing facebook credentiasl")
-    store_credentials(app_id, app_secret)
+@click.option("-v", "--view", is_flag=True, flag_value=True, help="Display current credentials")
+def creds(view):
+    if not view:
+        app_id = click.prompt("Facebook app_id: ")
+        app_secret = click.prompt("Facebook app_secret: ")
+        click.echo("Storing facebook credentiasl")
+        store_credentials(app_id, app_secret)
+    else:
+        creds = get_credentials()
+        click.echo(json.dumps(creds, indent=2))
 
 
 @click.command(help="Get a user long lived access token.")
@@ -43,4 +49,4 @@ def sendtext():
 def debugtoken():
     access_token = click.prompt("facebook access token: ")
     result = debug_access_token(access_token)
-    click.echo(result)
+    click.echo(json.dumps(result, indent=2))
